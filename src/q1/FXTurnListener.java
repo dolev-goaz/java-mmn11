4df8dca7458bc029e0a261b7cfcdb96c5a3560e1
dolev goaz <www.dolev@gmail.com>
2024-04-01 22:24:39 +0300

    better string formatting

--- src/q1/FXTurnListener.java
+++ src/q1/FXTurnListener.java
@@ -6,16 +6,16 @@ import java.util.stream.Collectors;
 public class FXTurnListener implements ITurnListener {
     @Override
     public void onTurnPlayed(Card firstPlayerCard, Card secondPlayerCard, int winner) {
-        System.out.println("Turn played!");
-        System.out.println(String.format("First player card- %s", firstPlayerCard));
-        System.out.println(String.format("Second player card- %s", secondPlayerCard));
+        System.out.println(String.format("Player 1 card- %s", firstPlayerCard));
+        System.out.println(String.format("Player 2 card- %s", secondPlayerCard));
+        System.out.println(String.format("Player %d won this round!\n", winner));
     }
 
     @Override
     public void onWarPlayed(ArrayList<Card> firstPlayerCards, ArrayList<Card> secondPlayerCards, int winner) {
-        System.out.println("War played!");
-        System.out.println(String.format("First player card- %s", joinCardsStr(firstPlayerCards)));
-        System.out.println(String.format("Second player card- %s", joinCardsStr(secondPlayerCards)));
+        System.out.println(String.format("Player 1 cards- %s", joinCardsStr(firstPlayerCards)));
+        System.out.println(String.format("Player 2 cards- %s", joinCardsStr(secondPlayerCards)));
+        System.out.println(String.format("Player %d won this round!\n", winner));
     }
 
     private String joinCardsStr(ArrayList<Card> cards) {
