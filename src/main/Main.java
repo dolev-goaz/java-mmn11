210da75bbfa7cb754d08f31e671227660850c9b6
dolev goaz <www.dolev@gmail.com>
2024-04-02 22:11:06 +0300

    seperated to seperate packages, begin q2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package q1;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("WarGame.fxml"));
-        primaryStage.setTitle("War Game");
+        // FOR q1
+//        Parent root = FXMLLoader.load(getClass().getResource("../q1/WarGame.fxml"));
+//        primaryStage.setTitle("War Game");
+        // FOR q2
+        Parent root = FXMLLoader.load(getClass().getResource("../q2/matrix.fxml"));
+        primaryStage.setTitle("Matrix");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
---
+++ src/q2/randomMatrixController.java
@@ -0,0 +1,17 @@
+package q2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+public class randomMatrixController {
+
+    @FXML
+    private Canvas matrixCanvas;
+
+    @FXML
+    void onRandomFill(ActionEvent event) {
+
+    }
+
+}
